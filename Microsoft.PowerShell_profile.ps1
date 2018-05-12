4fa000e5a8d30be73746ebef712e308c62ffa150
John D. Fisher <jdfenw@gmail.com>
2018-05-12 13:13:08 -0700

    feat: Add Set-Colorscheme
    Create cmdlet to switch between light and dark solarized themes.
    Include option to write defaults, which can be saved via Properties and
    Defaults in Window menu.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-set-alias vim "C:/Program Files/Vim/vim80/vim.exe"
+#set-alias vim "C:/Program Files/Vim/vim80/vim.exe"
 
 # To edit the Powershell Profile
 # (Not that I'll remember this)
@@ -13,6 +13,46 @@ Function Edit-Vimrc
     vim $HOME\_vimrc
 }
 
+# Set colorscheme
+Function Set-ColorScheme
+{
+    [cmdletbinding()]
+    Param(
+        [Parameter(Mandatory=$false)]
+        [switch] $Dark,
+
+        [Parameter(Mandatory=$false)]
+        [switch] $Light,
+
+        [Parameter(Mandatory=$false)]
+        [switch] $d
+    )
+    
+    Process
+    {
+        If ($Dark) {
+            $ColorToolScheme = "$PSScriptRoot\Solarized Dark Higher Contrast.itermcolors"
+        }
+        ElseIf ($Light) {
+            $ColorToolScheme = "$PSScriptRoot\Solarized Light.itermcolors"
+        }
+        Else {
+            $ColorToolScheme = "$PSScriptRoot\Solarized Dark Higher Contrast.itermcolors"
+        }
+        Write-Host $ColorToolScheme
+        $ColorTool = "C:\Users\fishe\Documents\GitHub\console\tools\ColorTool"
+        # $ColorToolScheme = "$ColorTool\schemes\Solarized Dark Higher Contrast.itermcolors"
+        # $ColorToolScheme = "C:\Users\fishe\Documents\GitHub\iTerm2-Color-Schemes\schemes\Solarized Dark Higher Contrast.itermcolors"
+        
+        $ColorToolExe = "$ColorTool\ColorTool.exe"
+        & $ColorToolExe  $ColorToolScheme
+        If ($d) {
+            & $ColorToolExe  -d $ColorToolScheme
+        }
+        Set-PSReadlineOption -ResetTokenColors
+    }
+}
+#Set-ColorScheme
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
