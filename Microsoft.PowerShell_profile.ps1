fba0eec19e22e8346b14e10003ced9bb331998d0
John D. Fisher <jdfenw@gmail.com>
2018-06-13 17:52:50 -0700

    feat(Set-ColorScheme) add shouldprocess

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -26,7 +26,7 @@ If -d is selected, the color scheme is written to the console defaults. To
 save the defaults for future sessions, select Properties and OK and select
 Defaults and OK from the console menu.
 #>
-    [cmdletbinding()]
+    [cmdletbinding(SupportsShouldProcess, ConfirmImpact='Low')]
     Param(
         [Parameter(Mandatory=$false)]
         [switch] $Dark,
@@ -37,6 +37,19 @@ Defaults and OK from the console menu.
         [Parameter(Mandatory=$false)]
         [switch] $d
     )
+
+     Begin {
+            if (-not $PSBoundParameters.ContainsKey('Verbose')) {
+                $VerbosePreference = $PSCmdlet.SessionState.PSVariable.GetValue('VerbosePreference')
+            }
+            if (-not $PSBoundParameters.ContainsKey('Confirm')) {
+                $ConfirmPreference = $PSCmdlet.SessionState.PSVariable.GetValue('ConfirmPreference')
+            }
+            if (-not $PSBoundParameters.ContainsKey('WhatIf')) {
+                $WhatIfPreference = $PSCmdlet.SessionState.PSVariable.GetValue('WhatIfPreference')
+            }
+            Write-Verbose ('[{0}] Confirm={1} ConfirmPreference={2} WhatIf={3} WhatIfPreference={4}' -f $MyInvocation.MyCommand, $Confirm, $ConfirmPreference, $WhatIf, $WhatIfPreference)
+        }
 
     Process
     {
@@ -49,16 +62,22 @@ Defaults and OK from the console menu.
         Else {
             $ColorToolScheme = "$PSScriptRoot\Solarized Dark Higher Contrast.itermcolors"
         }
-        Write-Host $ColorToolScheme
+        Write-Verbose "You selected for the current session:`n         $ColorToolScheme"
         # $ColorTool = "C:\Users\fishe\Documents\GitHub\console\tools\ColorTool"
         $ColorTool = "$PSScriptRoot"
         # $ColorToolScheme = "$ColorTool\schemes\Solarized Dark Higher Contrast.itermcolors"
         # $ColorToolScheme = "C:\Users\fishe\Documents\GitHub\iTerm2-Color-Schemes\schemes\Solarized Dark Higher Contrast.itermcolors"
 
         $ColorToolExe = "$ColorTool\ColorTool.exe"
+
+
         & $ColorToolExe  $ColorToolScheme
+
         If ($d) {
-            & $ColorToolExe  -d $ColorToolScheme
+            if ($PSCmdlet.ShouldProcess("Default Colorscheme")) {
+                Write-Verbose "Default updated to $ColorToolScheme`n Don't forget to save Defaults"
+                & $ColorToolExe  -d $ColorToolScheme
+            }
         }
 
         Set-PSReadlineOption -ResetTokenColors
@@ -101,12 +120,40 @@ function cddash {
     } else {
         $pwd = $args[0];
     }
-    $tmp = pwd;
+    $tmp = Get-Location;
 
     if ($pwd) {
         Set-Location $pwd;
     }
     Set-Variable -Name OLDPWD -Value $tmp -Scope global;
 }
-
 Set-Alias -Name cd -value cddash -Option AllScope
+
+Function Defender
+{
+<#
+.Synopsis
+Windows Defender Scan
+
+.Description
+Create a function to run MpCmdRun.exe with appropriate options to scan a file.
+
+Defender -Scan -ScanType 3 -File <filename>
+#>
+    [cmdletbinding()]
+    Param(
+        [Parameter(Mandatory=$true,Position=1)]
+        [string] $File
+    )
+
+    Begin
+    {
+        $Window_Defender = "$env:ProgramFiles\Windows Defender\MpCmdRun.exe"
+        $ArgumentList = "-Scan -ScanType 3 -File $File"
+    }
+
+    Process
+    {
+        Start-Process -FilePath $Window_Defender -ArgumentList $ArgumentList
+    }
+}
