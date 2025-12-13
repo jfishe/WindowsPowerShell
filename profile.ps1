e59e0392dde6e9fd9de0eaa680ac87518b9e5473
John D. Fisher <jdfenw@gmail.com>
2025-12-13 14:20:58 -0800

    feat(conda): use Invoke-Expression

--- profile.ps1
+++ profile.ps1
@@ -1,6 +1,8 @@
 
 #region conda initialize
 # !! Contents within this block are managed by 'conda init' !!
-(& "C:\Users\jdfen\miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
+If (Test-Path "$env:USERPROFILE\miniforge3\Scripts\conda.exe") {
+    (& "$env:USERPROFILE\miniforge3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
+}
+Enter-CondaEnvironment -Name vim-python
 #endregion
-
