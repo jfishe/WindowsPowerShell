c82e8065ad515341dd98db9e2035a4fbec040f33
John D. Fisher <jdfenw@gmail.com>
2025-12-14 12:26:21 -0800

    feat(cruft): remove unused
    Remove psscriptanalyzer suppression.
    
    Remove Chocolatey helpers.
    
    Remove Oh-My-Posh config.
    
    Generalize domain check.
    
    Remove Set-Dotfile and Set-ColorScheme.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,3 @@
-[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingInvokeExpression",
-    "", Justification = "Required by starship")]
-Param()
-
-# Chocolatey profile
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-    Import-Module "$ChocolateyProfile"
-}
-
 # PSReadline Settings
 If ($host.Name -eq 'ConsoleHost') {
     $PSReadlineOptions = @{
@@ -25,16 +15,6 @@ If ($host.Name -eq 'ConsoleHost') {
         # Prediction
         PredictionSource              = "History"
         PredictionViewStyle           = "ListView"
-
-        # Oh-My-Posh prompt
-        ExtraPromptLineCount          = 1
-        PromptText                    = "$([char]::ConvertFromUtf32(0x279C)) " # ➜
-
-        # Colors = @{
-        #     "InlinePredictionColor" = "`e[95m"
-        #     "ListPredictionColor" = "`e[95m"
-        #     "ListPredictionSelectedColor" = "`e[48;95m"
-        # }
     }
     Set-PSReadLineOption @PSReadlineOptions
     # Disabled by default in vi mode
@@ -53,7 +33,7 @@ If ($host.Name -eq 'ConsoleHost') {
     $Host.PrivateData.ProgressForegroundColor = [ConsoleColor]::Red
 }
 
-if ($env:USERDOMAIN -eq 'DOMAIN1') {
+if ("$env:USERDOMAIN" -ne "$(hostname)") {
     # Set these in $PROFILE to overide ~/.gitconfig:
     # GIT_AUTHOR_NAME is the human-readable name in the “author” field.
     $env:GIT_AUTHOR_NAME = 'John D. Fisher'
@@ -90,127 +70,10 @@ Function cddash {
 }
 Set-Alias -Name cd -value cddash -Option AllScope
 
-Function Set-Dotfile {
-    <#
-    .SYNOPSIS
-    Hide dotfiles (.*) similar to *nix shells.
-
-    .DESCRIPTION
-    Set the file attribute to Hidden for files beginning with a period (.*).
-
-    .PARAMETER Path
-    Full or relative Path to a directory or comma separated list of directories
-    containing dotfiles.
-
-    .PARAMETER Recurse
-    Recurse through sub-directories.
-
-    .PARAMETER Force
-    The name of a file to write failed computer names to. Defaults to errors.txt.
-      #>
-    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'Medium')]
-
-    Param(
-        [Parameter()]
-        [SupportsWildcards()]
-        [string[]] $Path = '.',
-        [Parameter()]
-        [switch] $Recurse,
-        [Parameter()]
-        [switch] $Force
-    )
-
-    Begin {
-        if (-not $PSBoundParameters.ContainsKey('Verbose')) {
-            $VerbosePreference = $PSCmdlet.SessionState.PSVariable.GetValue('VerbosePreference')
-        }
-        if (-not $PSBoundParameters.ContainsKey('Confirm')) {
-            $ConfirmPreference = $PSCmdlet.SessionState.PSVariable.GetValue('ConfirmPreference')
-        }
-        if (-not $PSBoundParameters.ContainsKey('WhatIf')) {
-            $WhatIfPreference = $PSCmdlet.SessionState.PSVariable.GetValue('WhatIfPreference')
-        }
-        Write-Verbose ('[{0}] Confirm={1} ConfirmPreference={2} WhatIf={3} WhatIfPreference={4}' -f $MyInvocation.MyCommand, $Confirm, $ConfirmPreference, $WhatIf, $WhatIfPreference)
-    }
-
-    Process {
-        <# Pre-impact code #>
-
-        # -Confirm --> $ConfirmPreference = 'Low'
-        # ShouldProcess intercepts WhatIf* --> no need to pass it on
-        if ($Force -or $PSCmdlet.ShouldProcess("ShouldProcess?")) {
-            Write-Verbose ('[{0}] Reached command' -f $MyInvocation.MyCommand)
-            # Variable scope ensures that parent session remains unchanged
-            $ConfirmPreference = 'None'
-        }
-
-        Get-ChildItem -Path $Path -Recurse:$Recurse -Force:$Force | `
-            Where-Object { $_.name -like ".*" -and $_.attributes -match 'Hidden' `
-                -eq $false } | `
-            Set-ItemProperty -name Attributes `
-            -value ([System.IO.FileAttributes]::Hidden)
-
-        <# Post-impact code #>
-    }
-}
-
 Function _which {
     Get-Command -All $Args[0] -ErrorAction SilentlyContinue | Format-List
 }
 Set-Alias -Name which -Value _which
-
-Function Set-ColorScheme {
-
-    <#
-.SYNOPSIS
-
-Toggle the console color scheme between solarized dark and light.
-
-.DESCRIPTION
-
-Windows Console ColorTool should be in $env:PATH.
-
-The schemes\ folder should be in the same directory as ColorTool.exe.
-
-The color schemes, based on vim-solarized8, were created using terminal.sexy.
-
-.OUTPUTS
-
-ColorTool.exe --quiet [[solarized.dark.itermcolors]|[solarized.light.itermcolors]]
-
-.LINK
-
-https://github.com/Microsoft/console/tree/master/tools/ColorTool
-
-.LINK
-
-https://terminal.sexy/
-
-.LINK
-
-https://github.com/lifepillar/vim-solarized8
-
-#>
-    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'Low')]
-    param()
-    begin {
-        $colortool = Get-Command -Name "colortool"
-        $ColorSchemes = $colortool.Path |
-        ForEach-Object -Process { (Get-Item $_).Directory } |
-        ForEach-Object -Process { Get-ChildItem $_ -Name "schemes/solarized.*" }
-        $colorscheme = [int]$(($env:COLORSCHEME -eq 0))
-        $ConfirmMessage = @("Change console color scheme to",
-            $ColorSchemes[$colorscheme]
-        )
-    }
-    process {
-        if ($PSCmdlet.ShouldProcess($ConfirmMessage)) {
-            $env:COLORSCHEME = $colorscheme
-            & $colortool --quiet $ColorSchemes[$env:COLORSCHEME]
-        }
-    }
-}
-Set-Alias -Name yob -Value Set-ColorScheme
 
 $env:PROFILEDIR = (Get-Item $PROFILE).Directory
 
